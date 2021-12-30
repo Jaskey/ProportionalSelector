8b1662ec76d0c0f048b68dc0d2bbe7f6fcc95164
Jaskey <linjunjie1103@gmail.com>
2021-12-30 16:47:55 +0800

    Update comments of SelectStrategy

--- src/com/github/jaskey/SelectStrategy.java
+++ src/com/github/jaskey/SelectStrategy.java
@@ -8,10 +8,12 @@ import java.util.Set;
  * 策略设计模式，用以实际选择候选实体
  */
 public interface SelectStrategy<T> {
+      
       T selectOne(Set<T> candidateSet);
 
       /**
-       * 按照权重挑选多个候选，适用于主备用的模式，例如三个候选服务商，需要按权重选择一个，但是这个如果这个服务商不可用，则需要重新选，那么就可以使用此方法，一下子选择两个候选的服务商
+       * 挑选多个候选
+       * 适用类似以下场景：三个候选服务商，需要按权重选择一个，但是我们需要保证一开始选择的供应商接口不可用的情况下，还能重新选，那么就可以使用此方法，一下子选择两个候选的服务商，
        * @return
        */
       List<T> select(Set<T> candidateSet, int selectCnt) ;
